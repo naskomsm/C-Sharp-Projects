a555d67554c72a5a3cb531b2463628cf2b39984a
Atanas Kolev <44707978+naskomsm@users.noreply.github.com>
2019-02-04 12:59:18 +0200

    Add files via upload

---
+++ Library/Data/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace Library.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=LibraryDb;Integrated Security=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
---
+++ Phonebook/Data/Models/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace Phonebook.Data.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
