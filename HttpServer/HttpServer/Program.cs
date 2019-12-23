f9a4c2eede007223ca12d5cb4820ddba0826e7b0
naskomsm <naskokolev00@gmail.com>
2019-12-23 13:39:52 +0200

    added users dictionary which is filled with username and password after post

--- HttpServer/HttpServer/Program.cs
+++ HttpServer/HttpServer/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace HttpServer
 {
     using System;
+    using System.Collections.Generic;
     using System.Net;
     using System.Net.Sockets;
     using System.Text;
@@ -14,15 +15,15 @@
             const string responseBody = "<form method='post'>" +
                 "<input type='text' name='username' placeholder='username'</input>" +
                 "<input type='password' name='password' placeholder='password'</input>" +
-                "<input type='submit'></input>" + 
+                "<input type='submit'></input>" +
                 "</form>";
 
+            var users = new Dictionary<string, string>();
             var port = 8000;
             var ipAdress = IPAddress.Loopback;
 
             var server = new TcpListener(ipAdress, port);
             server.Start();
-
 
             while (true)
             {
@@ -34,8 +35,26 @@
                     var readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
                     var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
 
+                    var splittedRequest = stringRequest.Split(Environment.NewLine);
+                    
                     Console.WriteLine(new string('=', 70));
                     Console.WriteLine(stringRequest);
+
+                    if (splittedRequest[0].Contains("POST"))
+                    {
+                        var nameAndPassword = splittedRequest[splittedRequest.Length - 1].Split("&");
+                        var username = nameAndPassword[0].Split("=")[1];
+                        var password = nameAndPassword[1].Split("=")[1];
+
+                        Console.WriteLine("Addd user with: ");
+                        Console.WriteLine($"Username: {username}");
+                        Console.WriteLine($"Password: {password}");
+
+                        if (!users.ContainsKey(username))
+                        {
+                            users.Add(username, password);
+                        }
+                    }
 
                     // text/html response
                     string response = "HTTP/1.0 200 OK" + NewLine +
@@ -55,6 +74,5 @@
                 }
             }
         }
-
     }
 }
