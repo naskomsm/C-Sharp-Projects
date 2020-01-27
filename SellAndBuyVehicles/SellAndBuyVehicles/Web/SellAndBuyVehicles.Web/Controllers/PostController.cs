c2e36b7f9835d743ffed33228be72439b9d73066
naskomsm <naskokolev00@gmail.com>
2020-01-27 14:45:51 +0200

    made post controller and finished its view

---
+++ SellAndBuyVehicles/SellAndBuyVehicles/Web/SellAndBuyVehicles.Web/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿namespace SellAndBuyVehicles.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
