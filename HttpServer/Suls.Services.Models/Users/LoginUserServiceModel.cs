e34afd7667f91d31738c302cec5dada39edd6ddb
naskomsm <naskokolev00@gmail.com>
2020-02-02 11:15:06 +0200

    upgraded old http server with SIS

--- HttpServer/Suls.Services.Models/Users/LoginUserServiceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Suls.Services.Models.Users
-{
-    public class LoginUserServiceModel
-    {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-    }
-}
