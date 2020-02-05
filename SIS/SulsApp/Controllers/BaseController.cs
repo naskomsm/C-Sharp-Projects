ee0e00c127e0a3c15f02038b7a03c1ad8cb50c5e
naskomsm <naskokolev00@gmail.com>
2020-02-05 16:57:52 +0200

    added new erroe viewmodel, custom attributes and identity user class. new html views

---
+++ SIS/SulsApp/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SulsApp.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+    }
+}
