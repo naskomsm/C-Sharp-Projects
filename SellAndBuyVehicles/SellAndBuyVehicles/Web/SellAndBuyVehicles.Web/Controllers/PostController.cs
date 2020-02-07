dc84a7ffcc5dfc05ef7fab419024e8b18fe09623
naskomsm <naskokolev00@gmail.com>
2020-02-07 13:13:59 +0200

    added models into all views

--- SellAndBuyVehicles/SellAndBuyVehicles/Web/SellAndBuyVehicles.Web/Controllers/PostController.cs
+++ SellAndBuyVehicles/SellAndBuyVehicles/Web/SellAndBuyVehicles.Web/Controllers/PostController.cs
@@ -15,7 +15,34 @@
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            var jsonStringCategories = System.IO.File.ReadAllText("./Datasets/Categories.json");
+            var jsonStringCities = System.IO.File.ReadAllText("./Datasets/Cities.json");
+            var jsonStringMakes = System.IO.File.ReadAllText("./Datasets/Makes.json");
+            var jsonStringYears = System.IO.File.ReadAllText("./Datasets/Years.json");
+            var jsonStringColors = System.IO.File.ReadAllText("./Datasets/Colors.json");
+            var jsonStringCarTypeCategories = System.IO.File.ReadAllText("./Datasets/CarTypeCategories.json");
+            var jsonStringCarFeatures = System.IO.File.ReadAllText("./Datasets/CarFeatures.json");
+
+            var parsedDataCategories = JsonConvert.DeserializeObject<string[]>(jsonStringCategories);
+            var parsedDataCities = JsonConvert.DeserializeObject<string[]>(jsonStringCities);
+            var parsedDataMakes = JsonConvert.DeserializeObject<string[]>(jsonStringMakes);
+            var parsedDataYears = JsonConvert.DeserializeObject<string[]>(jsonStringYears);
+            var parsedDataColors = JsonConvert.DeserializeObject<string[]>(jsonStringColors);
+            var parsedDataCarTypeCategories = JsonConvert.DeserializeObject<string[]>(jsonStringCarTypeCategories);
+            var parsedDataCarFeatures = JsonConvert.DeserializeObject<CarFeatures[]>(jsonStringCarFeatures);
+
+            var model = new SearchPageViewModel()
+            {
+                Categories = parsedDataCategories,
+                Cities = parsedDataCities,
+                Makes = parsedDataMakes,
+                Years = parsedDataYears,
+                Colors = parsedDataColors,
+                Features = parsedDataCarFeatures[0],
+                CarTypeCategory = parsedDataCarTypeCategories
+            };
+
+            return View(model);
         }
 
         [HttpGet]
