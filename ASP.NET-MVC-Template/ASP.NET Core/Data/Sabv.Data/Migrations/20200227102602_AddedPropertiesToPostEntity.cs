273f2933c1f8694cf567bb3f6c03c97a5bd3d80b
naskomsm <naskokolev00@gmail.com>
2020-02-27 15:18:41 +0200

    added some new properties to entities

---
+++ ASP.NET-MVC-Template/ASP.NET Core/Data/Sabv.Data/Migrations/20200227102602_AddedPropertiesToPostEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sabv.Data.Migrations
+{
+    public partial class AddedPropertiesToPostEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Make",
+                table: "Posts",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Posts",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Make",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Posts");
+        }
+    }
+}
---
+++ ASP.NET-MVC-Template/ASP.NET Core/Data/Sabv.Data/Migrations/20200227104756_AddedCurrencyEnumToPost.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sabv.Data.Migrations
+{
+    public partial class AddedCurrencyEnumToPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Currency",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Posts");
+        }
+    }
+}
